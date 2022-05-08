f8dc72e28f11490d358cd6b2667cc75e640f7e44
sadhityapratama <sadhityapratama@gmail.com>
2022-05-08 22:17:07 +0700

    template selesai

--- js/main.js
+++ js/main.js
@@ -43,6 +43,30 @@
         }
     })
 
+    $('.main-slider-no-loop').owlCarousel({
+        loop:false,
+        margin:0,
+        nav:false,
+        dots: false,
+        pagination: false,
+        autoHeight: false,
+        autoplay:false,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+//        autoHeightClass: 'owl-height',
+        navText : ["<img src='./img/slider/rightarrow.png' />","<img src='./img/slider/leftarrow.png' />"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
     
     $('.main-slider').owlCarousel({
         loop:true,
